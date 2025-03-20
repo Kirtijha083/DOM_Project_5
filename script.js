48686db363856e1db1dfcfe0439d742faec7a646
KirtiJha <kirti.jha2025@gmail.com>
2025-03-20 15:29:24 +0530

    some feature added

---
+++ script.js
@@ -0,0 +1,9 @@
+// array, object, array of object(json), for-each
+// adding stroy through js
+
+let arr = [1,2,3,5];
+
+arr.forEach(function (elem){
+    // console.log("hello");
+    console.log(elem);
+})
